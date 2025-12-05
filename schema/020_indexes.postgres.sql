7c4e6be63390e49369753f48fea17a049c3a3687
Vito416 <vito416@protonmail.com>
2025-12-05 16:32:59 +0100

    Regenerate PHP from schema

--- schema/020_indexes.postgres.sql
+++ schema/020_indexes.postgres.sql
@@ -2,6 +2,8 @@
 -- engine: postgres
 -- table:  peer_nodes
 
+CREATE UNIQUE INDEX IF NOT EXISTS ux_peer_nodes_name ON peer_nodes (name);
+
 CREATE INDEX IF NOT EXISTS idx_peer_nodes_status ON peer_nodes (status);
 
 CREATE INDEX IF NOT EXISTS idx_peer_nodes_last_seen ON peer_nodes (last_seen);
